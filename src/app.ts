4778cccb8323028c7ea747c3d863b240f3403c8c
Luiz de Freitas <luizdefreitas10@gmail.com>
2023-01-23 23:05:40 -0300

    corrigindo req 01

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import express from 'express';
+import carRouter from './Routes';
+import HandleError from './Middlewares/HandleError';
 
 const app = express();
 
 app.use(express.json());
+app.use(carRouter);
+app.use(HandleError.handle);
 
 export default app;
