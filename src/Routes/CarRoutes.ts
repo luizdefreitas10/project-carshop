711b8ace4479133890175d6737e855cabc20569c
Luiz de Freitas <luizdefreitas10@gmail.com>
2023-02-07 23:00:11 -0300

    corrigindo arquivo routes

--- src/Routes/CarRoutes.ts
+++ src/Routes/CarRoutes.ts
@@ -1,21 +1,23 @@
 import express from 'express';
 import CarController from '../Controllers/CarController';
 
+const CARS_ID_ROUTE = '/cars/:id';
+
 const carRouter = express.Router();
 
 carRouter.post('/cars', (req, res, next) =>
   new CarController(req, res, next).create());
 
-carRouter.get('/cars/:id', (req, res, next) =>
+carRouter.get(CARS_ID_ROUTE, (req, res, next) =>
   new CarController(req, res, next).findById());
 
 carRouter.get('/cars', (req, res, next) =>
   new CarController(req, res, next).findAll());
 
-carRouter.put('/cars/:id', (req, res, next) =>
+carRouter.put(CARS_ID_ROUTE, (req, res, next) =>
   new CarController(req, res, next).updateById());
 
-carRouter.delete('/cars/:id', (req, res, next) =>
+carRouter.delete(CARS_ID_ROUTE, (req, res, next) =>
   new CarController(req, res, next).delete());
 
 export default carRouter;
--- src/Routes/MotorcycleRoutes.ts
+++ src/Routes/MotorcycleRoutes.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import MotorcycleController from '../Controllers/MotorcycleController';
 
+const MOTORCYCLE_ID_ROUTE = '/motorcycles/:id';
+
 const motorcycleRouter = express.Router();
 
 motorcycleRouter.post('/motorcycles', (req, res, next) =>
@@ -9,13 +11,13 @@ motorcycleRouter.post('/motorcycles', (req, res, next) =>
 motorcycleRouter.get('/motorcycles', (req, res, next) =>
   new MotorcycleController(req, res, next).findAll());
 
-motorcycleRouter.get('/motorcycle/:id', (req, res, next) =>
+motorcycleRouter.get(MOTORCYCLE_ID_ROUTE, (req, res, next) =>
   new MotorcycleController(req, res, next).findById());
 
-motorcycleRouter.put('/motorcycles/:id', (req, res, next) =>
+motorcycleRouter.put(MOTORCYCLE_ID_ROUTE, (req, res, next) =>
   new MotorcycleController(req, res, next).updateById());
 
-motorcycleRouter.delete('/motorcycles/:id', (req, res, next) =>
+motorcycleRouter.delete(MOTORCYCLE_ID_ROUTE, (req, res, next) =>
   new MotorcycleController(req, res, next).delete());
 
 export default motorcycleRouter;
